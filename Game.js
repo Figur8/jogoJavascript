4da1df976ed00e04afa059af473cecb6411747b7
Ismael Carvalho <ismael_profissional@hotmail.com>
2019-11-21 16:41:33 -0300

    Funcionalidade das letras adicionado

--- Game.js
+++ Game.js
@@ -10,65 +10,34 @@ window.addEventListener('mousemove', this.keyReleased, false);
 window.addEventListener('keydown', this.getLetras, false);
 var letras = ['t', 'e', 's', 't', 'e'];
 var contador = 0;
-
 let personagem = new Personagem();
+var ponteiro;
+
 
 
 function gameLoop() {
     personagem.setPosX(personagem.getVelX() + personagem.getPosX());
     personagem.setPosY(personagem.getVelY() + personagem.getPosY());
     checarColisoes();
-    handlerEvents();
     render();
 }
 
 function escreverLetras(letra) {
-    if (letras[0] == letra) {
-        console.log("true");
-    };
-    console.log(letras);
-}
-
-function handlerEvents() {
-
-    atual = personagem.getParada();
-    if (teclaCima == true) {
-        atual = personagem.getCima();
-        personagem.setVelY(-3);
-        if (teclaDireita == true) {
-            atual = personagem.getDireitaCima();
-            personagem.setVelX(3);
-        }
-        if (teclaEsquerda == true) {
-            atual = personagem.getEsquerdaCima();
-            personagem.setVelX(-3);
-        }
-    } else if (teclaBaixo == true) {
-        atual = personagem.getBaixo();
-        personagem.setVelY(3);
-        if (teclaDireita == true) {
-            atual = personagem.getDireitaBaixo();
-            personagem.setVelX(3);
-        }
-        if (teclaEsquerda == true) {
-            atual = personagem.getEsquerdaBaixo();
-            personagem.setVelX(-3);
-        }
-    } else if (teclaEsquerda == true) {
-        atual = personagem.getEsquerda();
-        personagem.setVelX(-3);
-    } else if (teclaDireita == true) {
-        atual = personagem.getDireita();
-        personagem.setVelX(3);
+    if (letras.includes(letra)) {
+        console.log(letra + " foi removida", letras.indexOf(letra));
+        letras.splice(letras.indexOf(letra), 1);
+        console.log(letras);
     }
-
+    if (letras.length == 0) {
+        console.log("você venceu");
+    }
 }
 
 function checarColisoes() {
-    if (personagem.getPosX() + (personagem.getRaio() * 2) >= LARGURA_TELA || personagem.getPosX() <= 0) {
+    if (personagem.getPosX() + personagem.getRaio() / 2 >= LARGURA_TELA || personagem.getPosX() <= 0) {
         personagem.setPosX(personagem.getVelX() - personagem.getPosX());
     }
-    if (personagem.getPosY() + (personagem.getRaio() * 2) >= ALTURA_TELA || personagem.getPosY() <= 0) {
+    if (personagem.getPosY() + personagem.getRaio() / 2 >= ALTURA_TELA || personagem.getPosY() <= 0) {
         personagem.setPosY(personagem.getVelY() - personagem.getPosY());
     }
 }
@@ -76,46 +45,8 @@ function checarColisoes() {
 function getLetras(e) {
     var letra = e.key;
     console.log(letra);
+    console.log(letras);
     escreverLetras(letra);
-}
-
-function keyPressed(e) {
-    // console.log(e);
-    var code = e.keyCode;
-    switch (code) {
-        case 37:
-            teclaEsquerda = true;
-            break; //Left key
-        case 38:
-            teclaCima = true;
-            break; //Up key
-        case 39:
-            teclaDireita = true;
-            break; //Right key
-        case 40:
-            teclaBaixo = true;
-            break; //Down key
-    }
-}
-
-
-function keyReleased(e) {
-    // console.log(e);
-    var code = e.keyCode;
-    switch (code) {
-        case 37:
-            teclaEsquerda = false;
-            break; //Left key
-        case 38:
-            teclaCima = false;
-            break; //Up key
-        case 39:
-            teclaDireita = false;
-            break; //Right key
-        case 40:
-            teclaBaixo = false;
-            break; //Down key
-    }
 }
 
 setInterval(gameLoop, 17);
@@ -125,46 +56,28 @@ function mouseCoord(evt) {
     mousePosY = evt.clientY;
     mouseMove(evt.clientX, evt.clientY);
     escreverLetras();
-    console.log(move);
-
-
 }
 
 function mouseMove(positionX, positionY) {
-    // personagem.setPosX(positionX);
-    // personagem.setPosY(positionY);
     atual = personagem.getParada();
-    // SUBIR 
     if (positionY < personagem.getPosY()) {
         atual = personagem.getCima();
-        console.log("cima")
         personagem.setVelY(-3);
         if (positionX > personagem.getPosX()) {
-
-            console.log("cimaDireita")
             personagem.setVelX(3);
         }
         if (positionX < personagem.getPosX()) {
-            console.log("cimaEsquerda")
             personagem.setVelX(-3);
         }
     } else if (positionY > personagem.getPosY()) {
-        console.log("baixo")
+        atual = personagem.getCima();
         personagem.setVelY(3);
         if (positionX > personagem.getPosX()) {
-            console.log("baixoDireita")
             personagem.setVelX(3);
         }
         if (positionX < personagem.getPosX()) {
-            console.log("baixoEsquerda")
             personagem.setVelX(-3);
         }
     }
     escreverLetras();
-}
-
-function testeMouse() {
-    teste = false;
-    escreverLetras();
-    console.log(teste);
 }
--- Principal.js
+++ Principal.js
@@ -3,9 +3,8 @@ var context = canvas.getContext('2d');
 var LARGURA_TELA = canvas.width;
 var ALTURA_TELA = canvas.height;
 
-
 function render() {
-    context.fillStyle = "#ff00ff";
+    context.fillStyle = "#DCFF7D";
     context.fillRect(0, 0, LARGURA_TELA, ALTURA_TELA);
     context.drawImage(atual, posX, posY);
 }
